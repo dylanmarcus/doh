645c45b1c3941ea448668d2e0abfb7bda6ed08e7
Dylan Marcus <dylan@Silly-Dilly.hsd1.ca.comcast.net>
2024-05-16 14:19:09 -0700

    add number of balls and ball weight

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -37,15 +37,79 @@ const ingredients = [
 
 function Ingredients() {
   const { classes } = useStyles();
+  const [numberOfBalls, setNumberOfBalls] = useState(1);
+  const [ballWeight, setBallWeight] = useState(500);
   const [baseIngredientWeight, setBaseIngredientWeight] = useState(0);
-  const [ingredientPercentages, setIngredientPercentages] = useState(new Array(ingredients.length).fill(0));
+  const [ingredientPercentages, setIngredientPercentages] = useState([
+    numberOfBalls * ballWeight, // Default value for flour
+    60,                         // Default value for water
+    2,                          // Default value for salt
+    1,                          // Default value for yeast
+    10,                         // Default value for fat
+  ]);
 
   const handleInputFocus = (event) => {
     event.target.select();
   };
 
+  const handleNumberOfBallsChange = (event) => {
+    const value = parseInt(event.target.value);
+    setNumberOfBalls(value);
+    setBaseIngredientWeight(value * ballWeight);
+    updateIngredientPercentages(value * ballWeight);
+  };
+
+  const handleBallWeightChange = (event) => {
+    const value = parseInt(event.target.value);
+    setBallWeight(value);
+    setBaseIngredientWeight(numberOfBalls * value);
+    updateIngredientPercentages(numberOfBalls * value);
+  };
+
+  const updateIngredientPercentages = (flourWeight) => {
+    const updatedPercentages = [...ingredientPercentages];
+    for (let i = 1; i < ingredients.length; i++) {
+      updatedPercentages[i] = percentageToGrams(ingredientPercentages[i], flourWeight);
+    }
+    setIngredientPercentages(updatedPercentages);
+  };
+
   return (
     <Box>
+      <Grid className={classes.ingredientRow}>
+        <Grid.Col span={12}>
+          <Title order={5} className={classes.ingredientName}>Number of Balls</Title>
+        </Grid.Col>
+        <Grid.Col span={12} className={classes.centeredGrid}>
+          <div className={classes.inputWrapper}>
+            <TextInput
+              classNames={{ input: classes.input }}
+              styles={{ input: { textAlign: 'center' } }}
+              placeholder="Number of balls"
+              value={numberOfBalls}
+              onChange={handleNumberOfBallsChange}
+              onFocus={handleInputFocus}
+            />
+          </div>
+        </Grid.Col>
+      </Grid>
+      <Grid className={classes.ingredientRow}>
+        <Grid.Col span={12}>
+          <Title order={5} className={classes.ingredientName}>Ball Weight</Title>
+        </Grid.Col>
+        <Grid.Col span={12} className={classes.centeredGrid}>
+          <div className={classes.inputWrapper}>
+            <TextInput
+              classNames={{ input: classes.input }}
+              styles={{ input: { textAlign: 'center' } }}
+              placeholder="Ball weight"
+              value={ballWeight}
+              onChange={handleBallWeightChange}
+              onFocus={handleInputFocus}
+            />
+          </div>
+        </Grid.Col>
+      </Grid>
       {ingredients.map((ingredient, index) => (
         <Grid key={ingredient.name} className={classes.ingredientRow}>
           <Grid.Col span={12}>
@@ -62,27 +126,23 @@ function Ingredients() {
                 onFocus={handleInputFocus}
                 onChange={(event) => {
                   if (ingredient.isBaseIngredient) {
-                    setBaseIngredientWeight(parseFloat(event.target.value));
+                    setBaseIngredientWeight(parseInt(event.target.value));
                   } else {
                     const updatedPercentages = [...ingredientPercentages];
-                    updatedPercentages[index] = parseFloat(event.target.value);
+                    updatedPercentages[index] = parseInt(event.target.value);
                     setIngredientPercentages(updatedPercentages);
                   }
                 }}
               />
               {!ingredient.isBaseIngredient ? (
                 <TextInput
-                classNames={{ input: classes.input }}
-                styles={{ input: { textAlign: 'right' } }}
-                readOnly
-                value={
-                  ingredient.isBaseIngredient
-                    ? ''
-                    : percentageToGrams(ingredientPercentages[index], baseIngredientWeight).toFixed(2)
-                }
-                rightSection={<span>g</span>}
-              />
-              ) : (<></>)}
+                  classNames={{ input: classes.input }}
+                  styles={{ input: { textAlign: 'right' } }}
+                  readOnly
+                  value={ingredientPercentages[index].toFixed(2)}
+                  rightSection={<span>g</span>}
+                />
+              ) : null}
             </div>
           </Grid.Col>
         </Grid>
