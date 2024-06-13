025fa5778c188162131f0e336dc469da0611eabe
Dylan Marcus <dylan@Silly-Dilly.hsd1.ca.comcast.net>
2024-06-13 13:22:52 -0700

    Backend: Use CORS

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const cors = require('cors');
+app.use(cors());
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
