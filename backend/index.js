40116cd4c7e7d5a5a431b2abc0d9f93e2704c6bf
Dylan Marcus <dylan.marcus@icloud.com>
2024-06-21 16:43:00 -0700

    Backend: Define heroku post build. Serve static files in express

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const mongoose = require('mongoose');
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const cors = require('cors');
+// CORS configuration
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' ? 'https://doh-app-446575e57f3f.herokuapp.com' : 'http://localhost:5173',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
-const helmet = require('helmet');
+// Security and compression middleware
 app.use(helmet());
-
-const compression = require('compression');
 app.use(compression());
 
-const mongoose = require('mongoose');
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -28,10 +31,17 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use(express.json());
 
-// Routes
+// API Routes
 app.use('/api/auth', require('./routes/auth')); // Authentication routes
 app.use('/api/recipes', require('./routes/recipe')); // Recipe routes
 
-app.get('/', (req, res) => res.send('API is running...'));
+// Serve static files from the frontend's dist directory
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+// Handle SPA: redirect all non-API requests to the SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+});
+
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
