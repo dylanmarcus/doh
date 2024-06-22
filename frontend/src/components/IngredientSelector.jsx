977ebfcb5a0c511e231db86cd8480fb492d7ad6a
Dylan Marcus <dylan.marcus@icloud.com>
2024-06-21 20:41:52 -0700

    Frontend: IngredientSelector uses variable instead of json for default ingredients

--- frontend/src/components/IngredientSelector.jsx
+++ frontend/src/components/IngredientSelector.jsx
@@ -15,14 +15,79 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+const defaultIngredients = [
+  {
+    name: "flour",
+    label: "Flour",
+    unit: "g",
+    isBaseIngredient: true,
+    defaultPercentage: 0,
+    defaultSelected: true
+  },
+  {
+    name: "water",
+    label: "Water",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 60,
+    defaultSelected: true
+  },
+  {
+    name: "salt",
+    label: "Salt",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 2,
+    defaultSelected: true
+  },
+  {
+    name: "yeast",
+    label: "Yeast",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 1,
+    defaultSelected: true
+  },
+  {
+    name: "fat",
+    label: "Fat",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 10,
+    defaultSelected: true
+  },
+  {
+    name: "sugar",
+    label: "Sugar",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 5,
+    defaultSelected: false
+  },
+  {
+    name: "bakingSoda",
+    label: "Baking Soda",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 1,
+    defaultSelected: false
+  },
+  {
+    name: "bakingPowder",
+    label: "Baking Powder",
+    unit: "g",
+    isBaseIngredient: false,
+    defaultPercentage: 2,
+    defaultSelected: false
+  }
+];
+
 function IngredientSelector({ selectedIngredients, setSelectedIngredients, onClose }) {
   const { classes } = useStyles();
   const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
-    fetch('src/utils/ingredients.json')
-      .then(response => response.json())
-      .then(data => setIngredients(data));
+    setIngredients(defaultIngredients);
   }, []);
 
   const handleCheckboxChange = (index) => {
