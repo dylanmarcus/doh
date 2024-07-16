1b1f384e702fa17faab1d1158ec1f2f8a58e0756
Dylan Marcus <dylan.marcus@icloud.com>
2024-07-16 14:43:56 -0700

    Frontend: Move default ingredients into module to remove code repitition

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -4,6 +4,7 @@ import { notifications } from '@mantine/notifications';
 import { FaClipboardList, FaSave, FaTrash } from 'react-icons/fa';
 import { createStyles } from '@mantine/styles';
 import IngredientSelector from './IngredientSelector';
+import defaultIngredients from '../utils/defaultIngredients';
 import axios from 'axios';
 
 const useStyles = createStyles((theme) => ({
@@ -31,73 +32,6 @@ const useStyles = createStyles((theme) => ({
     justifyContent: 'center',
   },
 }));
-
-const defaultIngredients = [
-  {
-    name: "flour",
-    label: "Flour",
-    unit: "g",
-    isBaseIngredient: true,
-    defaultPercentage: 0,
-    defaultSelected: true
-  },
-  {
-    name: "water",
-    label: "Water",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 60,
-    defaultSelected: true
-  },
-  {
-    name: "salt",
-    label: "Salt",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 2,
-    defaultSelected: true
-  },
-  {
-    name: "yeast",
-    label: "Yeast",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 1,
-    defaultSelected: true
-  },
-  {
-    name: "fat",
-    label: "Fat",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 10,
-    defaultSelected: true
-  },
-  {
-    name: "sugar",
-    label: "Sugar",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 5,
-    defaultSelected: false
-  },
-  {
-    name: "bakingSoda",
-    label: "Baking Soda",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 1,
-    defaultSelected: false
-  },
-  {
-    name: "bakingPowder",
-    label: "Baking Powder",
-    unit: "g",
-    isBaseIngredient: false,
-    defaultPercentage: 2,
-    defaultSelected: false
-  }
-];
 
 const Recipe = ({ recipe, onRecipeSaved, navbarWidth, navbarOpened, userInitiatedReset, setUserInitiatedReset }) => {
   const { classes } = useStyles();
