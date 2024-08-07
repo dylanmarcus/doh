fe51124a3fe20700c1c7a221eb8851a401de6329
Dylan Marcus <dylan.marcus@icloud.com>
2024-08-07 16:03:41 -0700

    VERCEELLLLLLLLL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
-  proxy: {
-    '/api': {
-      target: process.env.VITE_API_URL || 'http://localhost:5000/api',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, '')
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:5000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
-  }
-},
+  },
 });
